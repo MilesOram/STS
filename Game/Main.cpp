3dec2528d3fbf9ad493f987b6947d6543a1353a1
MilesOram <86774698+MilesOram@users.noreply.github.com>
2023-07-15 13:12:25 +0100

    C++ code

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -12,8 +12,6 @@
 #include <string>
 #include <functional>
 
-namespace py = pybind11;
-
 #include "card.h"
 #include "character.h"
 #include "effects_triggers.h"
@@ -27,8 +25,10 @@ namespace py = pybind11;
 #include "savestate.h"
 #include "visuals.h"
 
+namespace py = pybind11;
 using namespace std;
-int callCount{ 0 };
+
+// load in and store pointers to all python functions needed for model use and training
 class pythonFunctions
 {
 private:
@@ -36,7 +36,7 @@ private:
     pybind11::module_ MicroModule;
     pybind11::module_ PredModule;
 
-    //MICRO
+    //MICRO (in-fight)
     unique_ptr<pybind11::function> takeStateGiveAction;
     unique_ptr<pybind11::function> takeNextStateUpdateNetwork;
     unique_ptr<pybind11::function> callQVALSforSim;
@@ -45,14 +45,14 @@ private:
     unique_ptr<pybind11::function> loadModel_micro;
     unique_ptr<pybind11::function> trainModel_micro;
 
-    //MACRO
+    //MACRO (out-of-fight)
     unique_ptr<pybind11::function> takeStatesUpdateNetwork;
     unique_ptr<pybind11::function> callExpectedforSim;
     unique_ptr<pybind11::function> getBestExpectedIndex;
     unique_ptr<pybind11::function> getBestExpectedValue;
     unique_ptr<pybind11::function> loadModel_macro;
 
-    //PRED
+    //PRED (prediction)
     unique_ptr<pybind11::function> updatePred;
     unique_ptr<pybind11::function> callAllPredSingle;
     unique_ptr<pybind11::function> callAllPredBatch;
@@ -87,27 +87,28 @@ public:
         callAllPredBatch = make_unique<pybind11::function>(PredModule.attr("callAllPredB"));
         loadModel_pred = make_unique<pybind11::function>(PredModule.attr("loadModel"));
     }
-    pybind11::function& ACTION_MICRO() { ++callCount;  return *takeStateGiveAction.get(); }
+    pybind11::function& ACTION_MICRO() { return *takeStateGiveAction.get(); }
     pybind11::function& UPDATE_MICRO() { return *takeNextStateUpdateNetwork.get(); }
-    pybind11::function& QSIM_MICRO() { ++callCount; return *callQVALSforSim.get(); }
-    pybind11::function& QALL_MICRO() { ++callCount; return *callAllQVALS.get(); }
-    pybind11::function& BESTQ_MICRO() { ++callCount; return *getBestQValIndex.get(); }
+    pybind11::function& QSIM_MICRO() { return *callQVALSforSim.get(); }
+    pybind11::function& QALL_MICRO() { return *callAllQVALS.get(); }
+    pybind11::function& BESTQ_MICRO() { return *getBestQValIndex.get(); }
     pybind11::function& LOAD_MICRO() { return *loadModel_micro.get(); }
     pybind11::function& TRAIN_MICRO() { return *trainModel_micro.get(); }
 
     pybind11::function& UPDATE_MACRO() { return *takeStatesUpdateNetwork.get(); }
-    pybind11::function& EXPSIM_MACRO() { ++callCount; return *callExpectedforSim.get(); }
-    pybind11::function& BESTEXP_MACRO() { ++callCount; return *getBestExpectedIndex.get(); }
-    pybind11::function& BESTEXPVAL_MACRO() { ++callCount; return *getBestExpectedValue.get(); }
+    pybind11::function& EXPSIM_MACRO() { return *callExpectedforSim.get(); }
+    pybind11::function& BESTEXP_MACRO() { return *getBestExpectedIndex.get(); }
+    pybind11::function& BESTEXPVAL_MACRO() { return *getBestExpectedValue.get(); }
     pybind11::function& LOAD_MACRO() { return *loadModel_macro.get(); }
 
     pybind11::function& UPDATE_PRED() { return *updatePred.get(); }
-    pybind11::function& CALL_S_PRED() { ++callCount; return *callAllPredSingle.get(); }
-    pybind11::function& CALL_B_PRED() { ++callCount; return *callAllPredBatch.get(); }
+    pybind11::function& CALL_S_PRED() { return *callAllPredSingle.get(); }
+    pybind11::function& CALL_B_PRED() { return *callAllPredBatch.get(); }
     pybind11::function& LOAD_PRED() { return *loadModel_pred.get(); }
 
 };
 
+// takes any number of micro states, returns the index of the state with the highest Q-Value
 int callMicroIndex(const std::vector<stateInfo>& vecSINFOS, pythonFunctions& pyFunc_)
 {
     vector<vector<vector<int>>> allSimmedSINFO;
@@ -120,6 +121,7 @@ int callMicroIndex(const std::vector<stateInfo>& vecSINFOS, pythonFunctions& pyF
     return pyFunc_.BESTQ_MICRO()(allSimmedSINFO, allSimmedSINFOMasks).cast<int>();
 
 }
+// takes any number of micro states, returns a vector of Q-Values for each state
 vector<float> callMicroValues(const std::vector<stateInfo>& vecSINFOS, pythonFunctions& pyFunc_)
 {
     vector<vector<vector<int>>> allSimmedSINFO;
@@ -132,15 +134,15 @@ vector<float> callMicroValues(const std::vector<stateInfo>& vecSINFOS, pythonFun
     return pyFunc_.QALL_MICRO()(allSimmedSINFO, allSimmedSINFOMasks).cast<vector<float>>();
 }
 
-
+// displays Q-Values of each possible action, used for evaluating model shortcomings whilst manually playing
 void showAIDecisions(pythonFunctions& pyFunc_)
 {
-    //if (ai) return;
     stateInfo SINFO;
     SINFO.createAllVars();
     pyFunc_.ACTION_MICRO()(SINFO.getNormalInputs(), SINFO.getMasks(), SINFO.getEmbeddingInputsCA(), SINFO.getEmbeddingInputsEN(),SINFO.getEmbeddingInputsPO(), true, 27); // 27 prints it
 }
 
+// takes any number of macro states, returns index of state with highest expected end floor - CONSTPRED means comparing states with the same map path, no need to recalculate PRED for each
 int callMacroIndexCONSTPRED(const vector<macroStateInfo>& MSIs, pythonFunctions& pyFunc_)
 {
     if (MSIs.size() == 0) return 0;
@@ -154,6 +156,7 @@ int callMacroIndexCONSTPRED(const vector<macroStateInfo>& MSIs, pythonFunctions&
     }
     return pyFunc_.BESTEXP_MACRO()(pathVars, pathPreds).cast<int>();
 }
+// takes any number of macro states, returns index of state with highest expected end floor - REROLLPRED means comparing states with different map paths, need to recalculate PRED for each
 int callMacroIndexREROLLPRED(const vector<macroStateInfo>& MSIs, pythonFunctions& pyFunc_)
 {
     if (MSIs.size() == 0) return 0;
@@ -167,16 +170,19 @@ int callMacroIndexREROLLPRED(const vector<macroStateInfo>& MSIs, pythonFunctions
     }
     return pyFunc_.BESTEXP_MACRO()(pathVars, pathPreds).cast<int>();
 }
+// returns expected end floor for a given macro state
 float callMacroSINGLE(const macroStateInfo& MSI, pythonFunctions& pyFunc_)
 {
     if (gm.getRandomMacroThisFloor()) return random_float(0.0f, 50.0f);
 
     return pyFunc_.EXPSIM_MACRO()(MSI.getNormalInputs(),callPredSINGLE(MSI,pyFunc_)).cast<float>();
 }
+// returns all expected end healths for a single macro state
 vector<float> callPredSINGLE(const macroStateInfo& MSI, pythonFunctions& pyFunc_)
 {
     return pyFunc_.CALL_S_PRED()(MSI.getPredInputs()).cast<vector<float>>();
 }
+// returns all expected end healths for all input macro states
 vector<vector<float>> callPredBATCH(const vector<macroStateInfo>& MSIs, pythonFunctions& pyFunc_)
 {
     vector<vector<int>> pathPreds;
@@ -186,6 +192,7 @@ vector<vector<float>> callPredBATCH(const vector<macroStateInfo>& MSIs, pythonFu
     }
     return pyFunc_.CALL_B_PRED()(pathPreds).cast<vector<vector<float>>>();
 }
+// returns highest end floor from input macro states with the same map path
 float callMacroValueCONSTPRED(const vector<macroStateInfo>& MSIs, pythonFunctions& pyFunc_)
 {
     if (MSIs.size() == 0) return 0;
@@ -199,6 +206,7 @@ float callMacroValueCONSTPRED(const vector<macroStateInfo>& MSIs, pythonFunction
     }
     return pyFunc_.BESTEXPVAL_MACRO()(pathVars, pathPreds).cast<float>();
 }
+// returns highest end floor from input macro states with different map paths
 float callMacroValueREROLLPRED(const vector<macroStateInfo>& MSIs, pythonFunctions& pyFunc_)
 {
     if (MSIs.size() == 0) return 0;
@@ -213,13 +221,16 @@ float callMacroValueREROLLPRED(const vector<macroStateInfo>& MSIs, pythonFunctio
     return pyFunc_.BESTEXPVAL_MACRO()(pathVars, pathPreds).cast<float>();
 }
 
-bool ai{ true };
-bool fullRuns{ false };
+bool ai{ true }; // false for manual player control, true for full model piloting
+bool fullRuns{ false }; // false for micro training, true for macro training
+
 all_pc_info apci;
 gamestate gm;
 savestate sv;
-float rewardMultiplier{ 0.04f };
+float rewardMultiplier{ 0.04f }; // weighting of relic counters at fight end
 
+// similar to gamestate::outerTurnLoop, needed more flexibility for model integration
+// carries out the decision making and simulation in fights
 void outerTurnLoopAI(pythonFunctions& pyFunc)
 {
     int startHP = apci.getPlayer().getHealth();
@@ -246,8 +257,6 @@ void outerTurnLoopAI(pythonFunctions& pyFunc)
     bool showThisFight = random_int(1, 50) == 50;
     while (true)
     {
-        //apci.initVisuals();
-        //showAIDecisions(gm.getPyFunc());
         if (gm.getMapPathEvent() == 'B' && gm.getActNumber()>1 || showThisFight)
         {
             apci.initVisuals();
@@ -279,7 +288,7 @@ void outerTurnLoopAI(pythonFunctions& pyFunc)
         auto nextStateCA = SINFO.getEmbeddingInputsCA();
         auto nextStateEN = SINFO.getEmbeddingInputsEN();
         auto nextStatePO = SINFO.getEmbeddingInputsPO();
-        //int oldHealth = apci.getPlayer().getHealth();
+        
         int oldMaxHP = apci.getPlayer().getMaxHealth();
         int oldBlock = apci.getPlayer().getBlock();
         int oldEnergy = gm.getPlayerEnergy();
@@ -411,13 +420,15 @@ int main()
     gm.genRealMap();
     
     apci.initIronclad();
-
+    
     if (!ai)
     {
+        // plays a manually, player controlled game and exits
         gm.StartGameV();
         return 0;
     }
-
+    
+    // model training from here
     const int n_episodes{ 30003 };
     bool predTraining{ false };
     if (fullRuns)
@@ -427,7 +438,6 @@ int main()
         pyFunc.LOAD_PRED()();
         gm.holdPyFunc(pyFunc);
         vector<pair<int,int>> endFloors;
-        //gm.ResetRun();
         for (int e{ 0 }; e < n_episodes; ++e)
         {
             try
@@ -510,20 +520,23 @@ int main()
         pyFunc.LOAD_MACRO()();
         gm.holdPyFunc(pyFunc);
         gm.zeroMSI();
+        
         stateInfo SINFO;
         macroStateInfo PRED_INFO;
         vector<int> endPLHealths;
         map<string, vector<int>> endPLHealthsByFight;
         vector<statistics> stats;
         bool firstPass{ true };
+        
+        // allows training via human inputs as opposed to model decisions
         bool humanFeedback{ false };
+        
         try {
             gm.StartGame();
             for (int e{ 0 }; e < n_episodes; ++e)
             {
                 apci.masterDeckMaker(curriculumStage::Stage5,e);
                 gamestate::aiEventActionBuffer.clear();
-                //statistics currentStats(gm.makeDGEnum(), {apci.deckForStats()}, { apci.relicForStats() }, { apci.potionsForStats() }, apci.getDeck().size(), apci.getPlayer().getHealth());
                 gm.setBossIndex();
                 gm.playNextEvent();
                 if (predTraining) PRED_INFO.createPredVars();
@@ -589,7 +602,7 @@ int main()
                     auto nextStateCA = SINFO.getEmbeddingInputsCA();
                     auto nextStateEN = SINFO.getEmbeddingInputsEN();
                     auto nextStatePO = SINFO.getEmbeddingInputsPO();
-                    //int oldHealth = apci.getPlayer().getHealth();
+                    
                     int oldMaxHP = apci.getPlayer().getMaxHealth();
                     int oldBlock = apci.getPlayer().getBlock();
                     int oldEnergy = gm.getPlayerEnergy();
@@ -631,7 +644,6 @@ int main()
                         vector<float> Q_VALS;
                         vector<vector<vector<int>>> allSimmedSINFO;
                         vector<vector<bool>> allSimmedSINFOMasks;
-                        //float maximumQ{ -1000 };
                         for (int i{ 0 }; i < vecSINFOS.size(); ++i)
                         {
                             allSimmedSINFO.push_back({ vecSINFOS[i].getEmbeddingInputsCA(), vecSINFOS[i].getEmbeddingInputsEN(), vecSINFOS[i].getEmbeddingInputsPO(), vecSINFOS[i].getNormalInputs() });
@@ -708,65 +720,11 @@ int main()
                         std::cout << "episode:" << e << "/" << n_episodes << "      playerHealth:" << apci.getPlayer().getHealth() << "      " << fightName << ":" << nmeHP << std::endl;
                         endPLHealths.push_back(apci.getPlayer().getHealth());
                         endPLHealthsByFight[fightName].push_back(apci.getPlayer().getHealth());
-                        //currentStats.HP = apci.getPlayer().getHealth();
                         int hprng = random_int(75, 85);
                         apci.getPlayer().setMaxHealth(hprng);
                         apci.getPlayer().setHealth(hprng - random_int(0,20));
 
-                        //stats.push_back(currentStats);
-                        /*if (stats.size() % 1000 == 0)
-                        {
-                            for (int i{ 0 }; i < stats.size(); ++i)
-                            {
-                                ofstream file;
-                                switch (stats[i].dg)
-                                {
-                                case deckGen::EH1:
-                                    file.open("./model/sts/statsEH1.txt", ios::app);
-                                    break;
-                                case deckGen::HH1:
-                                    file.open("./model/sts/statsHH1.txt", ios::app);
-                                    break;
-                                case deckGen::EL1:
-                                    file.open("./model/sts/statsEL1.txt", ios::app);
-                                    break;
-                                case deckGen::BO1:
-                                    file.open("./model/sts/statsBO1.txt", ios::app);
-                                    break;
-                                case deckGen::EH2:
-                                    file.open("./model/sts/statsEH2.txt", ios::app);
-                                    break;
-                                case deckGen::HH2:
-                                    file.open("./model/sts/statsHH2.txt", ios::app);
-                                    break;
-                                case deckGen::EL2:
-                                    file.open("./model/sts/statsEL2.txt", ios::app);
-                                    break;
-                                case deckGen::BO2:
-                                    file.open("./model/sts/statsBO2.txt", ios::app);
-                                    break;
-                                }
-                                for (int j{ 0 }; j < stats[i].deckIDs.size(); ++j)
-                                {
-                                    file << stats[i].deckIDs[j] << " ";
-                                }
-                                file << "\n";
-                                for (int j{ 0 }; j < stats[i].relicIDs.size(); ++j)
-                                {
-                                    file << stats[i].relicIDs[j] << " ";
-                                }
-                                file << "\n";
-                                for (int j{ 0 }; j < stats[i].potionIDs.size(); ++j)
-                                {
-                                    file << stats[i].potionIDs[j] << " ";
-                                }
-                                file << "\n";
-                                file << stats[i].deckSize << " " << stats[i].HP;
-                                file << "\n";
-                                file.close();
-                            }
-                            stats.clear();
-                        }*/
+                        
                         break;
                     }
                 }
@@ -774,22 +732,6 @@ int main()
         }
         catch (py::error_already_set& e) {
             std::cout << e.what() << std::endl;
-        }
-        ofstream myfile2;
-        myfile2.open("./model/sts/FINALHPs.txt");
-        for (int i{ 0 }; i < endPLHealths.size(); ++i)
-        {
-            myfile2 << endPLHealths[i] << " " << i << "\n";
-        }
-        myfile2.close();
-        for (auto itr{ endPLHealthsByFight.begin() }; itr != endPLHealthsByFight.end(); ++itr)
-        {
-            ofstream myfile3;
-            myfile3.open("./model/sts/EndHealths_" + itr->first + ".txt");
-            for (int i{ 0 }; i < itr->second.size(); ++i)
-            {
-                myfile3 << itr->second[i] << " " << i << "\n";
-            }
         }
     }
     return 0;
